425e0cdeb47751622c66cfda6ac0df3993adad55
VivianaMurillom <viviana.zs104@gmail.com>
2022-05-18 11:32:58 -0500

    Creation of Header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './Header';
 import Navbar from './Navbar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <Navbar />
   </React.StrictMode>
 );
