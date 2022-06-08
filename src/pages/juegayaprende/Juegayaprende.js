8749c44f57a9ed45725f065cb589d0fa0fe96906
VivianaMurillom <viviana.zs104@gmail.com>
2022-06-08 11:43:35 -0500

    Adding routes to main menu jovenes and sub menu jovenes

--- src/pages/juegayaprende/Juegayaprende.js
+++ src/pages/juegayaprende/Juegayaprende.js
@@ -1,6 +1,7 @@
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
+import Mmenujovenes from '../../components/main-menu-jovenes/Mmenujovenes';
 
 function Juegayaprende(){
     return(
@@ -8,6 +9,8 @@ function Juegayaprende(){
         <Header/>
         <Navbar/>
         <h1>Juega y aprende</h1>
+        <p>Menu interno temporal</p>
+        <Mmenujovenes/>
         <Footer/>
        </div>
     );
